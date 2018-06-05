e34b81a91f4009cd70f08a16e4c289b78ba1d47b
PubliHero <e.makoviy@lsd.kiev.ua>
2018-06-05 12:12:56 +0300

    lesson_2

---
+++ lesson 2/HW_2.py
@@ -0,0 +1,19 @@
+a = int(input('Введите первую переменную:'))
+b = int(input('Введите вторую переменную:'))
+c = int(input('Введите третью переменную:'))
+if a == b :
+    print(a + 5)
+    print(b + 5)
+    print(c + 5)
+elif c == b:
+        print(a + 5)
+        print(b + 5)
+        print(c + 5)
+elif a == c:
+            print(a + 5)
+            print(b + 5)
+            print(c + 5)
+else:
+    print('Равных нет')
+
+
